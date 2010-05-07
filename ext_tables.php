44a9f35590f6ad1d4e81e076a4212c9404a35946
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-07 18:47:10 +0000

    1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagecarousel/tags/1.0.0@33037 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -21,6 +21,38 @@ $GLOBALS['TCA']['tt_content']['palettes']['7']['showitem'] .= ',tx_imagecarousel
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform,image_zoom';
 
+
+
+// Load fields for DAM
+if (t3lib_extMgm::isLoaded("dam")) {
+	$tempColumns = array(
+		'tx_jfdam_link' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:imagecarousel/locallang_db.xml:tx_dam.tx_jfdam_link',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
+				'wizards' => array(
+					'_PADDING' => 2,
+					'link' => array(
+						'type' => 'popup',
+						'title' => 'Link',
+						'icon' => 'link_popup.gif',
+						'script' => 'browse_links.php?mode=wizard',
+						'JSopenParams' => 'height=600,width=500,status=0,menubar=0,scrollbars=1'
+					)
+				)
+			)
+		),
+	);
+	t3lib_div::loadTCA('tx_dam');
+	t3lib_extMgm::addTCAcolumns('tx_dam', $tempColumns, 1);
+	t3lib_extMgm::addToAllTCAtypes('tx_dam', '--div--;LLL:EXT:dam/locallang_db.xml:tx_dam_item.div_custom, tx_jfdam_link;;;;1-1-1');
+	// add fields to index preset fields
+	$TCA['tx_dam']['txdamInterface']['index_fieldList'] .= ',tx_jfdam_link';
+}
+
+
 t3lib_extMgm::addPlugin(array(
 	'LLL:EXT:imagecarousel/locallang_db.xml:tt_content.list_type_pi1',
 	$_EXTKEY . '_pi1',
--- ext_tables.sql
+++ ext_tables.sql
@@ -4,3 +4,12 @@
 CREATE TABLE tt_content (
 	tx_imagecarousel_activate tinyint(3) DEFAULT '0' NOT NULL
 );
+
+
+
+#
+# Table structure for table 'tx_dam'
+#
+CREATE TABLE tx_dam (
+	tx_jfdam_link varchar(255) DEFAULT '' NOT NULL
+);
