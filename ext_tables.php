bb79774ba68f842c878e34f8887b14acf69f9597
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-23 18:59:49 +0000

    Add jQuery 1.6.1 / Add jCarousel 0.2.8
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagecarousel/trunk@48048 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -62,7 +62,7 @@ if (t3lib_extMgm::isLoaded("dam")) {
 t3lib_extMgm::addPlugin(array(
 	'LLL:EXT:imagecarousel/locallang_db.xml:tt_content.list_type_pi1',
 	$_EXTKEY . '_pi1',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'pi1/ce_icon.gif'
 ),'list_type');
 
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform_ds.xml');
@@ -79,7 +79,7 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/cloudcarousel/', 'Cloud-Carousel');
 t3lib_extMgm::addPlugin(array(
 	'LLL:EXT:imagecarousel/locallang_db.xml:tt_content.list_type_pi2',
 	$_EXTKEY . '_pi2',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'pi2/ce_icon.gif'
 ),'list_type');
 
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi2', 'FILE:EXT:'.$_EXTKEY.'/pi2/flexform_ds.xml');
