49f67df66c750643c75065fb29cd215e6bafb4ea
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-04 20:18:46 +0000

    Add jQuery 1.6.2 / Fix jsInFooter / Convert linefeed to Unix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagecarousel/trunk@50579 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_imagecarousel_itemsProcFunc.php
+++ lib/class.tx_imagecarousel_itemsProcFunc.php
@@ -1,1 +1,104 @@
-<?php/*************************************************************** *  Copyright notice * *  (c) 2009 Juergen Furrer <juergen.furrer@gmail.com> *  All rights reserved * *  This script is part of the TYPO3 project. The TYPO3 project is *  free software; you can redistribute it and/or modify *  it under the terms of the GNU General Public License as published by *  the Free Software Foundation; either version 2 of the License, or *  (at your option) any later version. * *  The GNU General Public License can be found at *  http://www.gnu.org/copyleft/gpl.html. * *  This script is distributed in the hope that it will be useful, *  but WITHOUT ANY WARRANTY; without even the implied warranty of *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the *  GNU General Public License for more details. * *  This copyright notice MUST APPEAR in all copies of the script! ***************************************************************/require_once (PATH_t3lib . 'class.t3lib_page.php'); /** * 'itemsProcFunc' for the 'imagecarousel' extension. *  * @author     Juergen Furrer <juergen.furrer@gmail.com> * @package    TYPO3 * @subpackage tx_imagecarousel */class tx_imagecarousel_itemsProcFunc{	/**	 * Get defined Skin for drupdown	 * @return array	 */	function getSkins($config, $item)	{		$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imagecarousel']);		if (! is_dir(t3lib_div::getFileAbsFileName($confArr['skinFolder']))) {			// if the defined folder does not exist, define the default folder			$confArr['skinFolder'] = "EXT:imagecarousel/res/skins/";		}		$items = t3lib_div::get_dirs(t3lib_div::getFileAbsFileName($confArr['skinFolder']));		if (count($items) > 0) {			$optionList = array();			foreach ($items as $key => $item) {				$item = trim($item);				if (! preg_match('/^\./', $item)) {					$optionList[] = array(						$item,						$item					);				}			}			$config['items'] = array_merge($config['items'], $optionList);		}		return $config;	}	/**	 * Get all modes for image selection	 * @return array	 */	function getModes($config, $item)	{		$optionList = array();		$optionList[] = array(			$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.upload'),			"upload",			"EXT:imagecarousel/mode_upload.gif"		);		$optionList[] = array(			$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.rte'),			"uploadRTE",			"EXT:imagecarousel/mode_rte.gif"		);		if (t3lib_extMgm::isLoaded("dam")) {			$optionList[] = array(				$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.dam'),				"dam",				"EXT:imagecarousel/mode_dam.gif"			);			if (t3lib_extMgm::isLoaded("dam_catedit")) {				$optionList[] = array(					$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.dam_catedit'),					"dam_catedit",					"EXT:imagecarousel/mode_damcat.gif"				);			}		}		$config['items'] = array_merge($config['items'], $optionList);		return $config;	}}if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/imagecarousel/lib/class.tx_imagecarousel_itemsProcFunc.php']) {	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/imagecarousel/lib/class.tx_imagecarousel_itemsProcFunc.php']);}?>
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 Juergen Furrer <juergen.furrer@gmail.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+require_once (PATH_t3lib . 'class.t3lib_page.php');
+
+ /**
+ * 'itemsProcFunc' for the 'imagecarousel' extension.
+ * 
+ * @author     Juergen Furrer <juergen.furrer@gmail.com>
+ * @package    TYPO3
+ * @subpackage tx_imagecarousel
+ */
+class tx_imagecarousel_itemsProcFunc
+{
+	/**
+	 * Get defined Skin for drupdown
+	 * @return array
+	 */
+	function getSkins($config, $item)
+	{
+		$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imagecarousel']);
+		if (! is_dir(t3lib_div::getFileAbsFileName($confArr['skinFolder']))) {
+			// if the defined folder does not exist, define the default folder
+			$confArr['skinFolder'] = "EXT:imagecarousel/res/skins/";
+		}
+		$items = t3lib_div::get_dirs(t3lib_div::getFileAbsFileName($confArr['skinFolder']));
+		if (count($items) > 0) {
+			$optionList = array();
+			foreach ($items as $key => $item) {
+				$item = trim($item);
+				if (! preg_match('/^\./', $item)) {
+					$optionList[] = array(
+						$item,
+						$item
+					);
+				}
+			}
+			$config['items'] = array_merge($config['items'], $optionList);
+		}
+		return $config;
+	}
+
+
+	/**
+	 * Get all modes for image selection
+	 * @return array
+	 */
+	function getModes($config, $item)
+	{
+		$optionList = array();
+		$optionList[] = array(
+			$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.upload'),
+			"upload",
+			"EXT:imagecarousel/mode_upload.gif"
+		);
+		$optionList[] = array(
+			$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.rte'),
+			"uploadRTE",
+			"EXT:imagecarousel/mode_rte.gif"
+		);
+		if (t3lib_extMgm::isLoaded("dam")) {
+			$optionList[] = array(
+				$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.dam'),
+				"dam",
+				"EXT:imagecarousel/mode_dam.gif"
+			);
+			if (t3lib_extMgm::isLoaded("dam_catedit")) {
+				$optionList[] = array(
+					$GLOBALS['LANG']->sL('LLL:EXT:imagecarousel/locallang_db.xml:tt_content.pi_flexform.mode.I.dam_catedit'),
+					"dam_catedit",
+					"EXT:imagecarousel/mode_damcat.gif"
+				);
+			}
+		}
+		$config['items'] = array_merge($config['items'], $optionList);
+		return $config;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/imagecarousel/lib/class.tx_imagecarousel_itemsProcFunc.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/imagecarousel/lib/class.tx_imagecarousel_itemsProcFunc.php']);
+}
+?>
--- res/jquery/js/imagecarousel-1.4.1.js
+++ res/jquery/js/imagecarousel-1.4.1.js
@@ -1,1 +1,51 @@
-var imagecarousel = {	itemVisibleInCallback: function(carousel, item, i, state, evt) {		var items = jQuery.data(carousel, 'items');		var idx = carousel.index(i, items.length);		carousel.add(i, items[idx - 1].html());	},	itemVisibleOutCallback: function(carousel, item, i, state, evt) {		carousel.remove(i);	},	randomize: function(carousel, childElem) {		var childes = childElem.split(',');		var lastChild = childes.pop();		var $this = jQuery(carousel + ' ' + childes.join(' '));		var elems = $this.children(lastChild);		elems.sort(function() { return (Math.round(Math.random())-0.5); });		$this.remove(lastChild);		for(var i=0; i < elems.length; i++) {			$this.append(elems[i]);		}	},	/**	 * Normal initCallback	 */	initCallback: function(id, carousel, state) {		var items = [];		for (a=0; a<carousel.options.size;a++) {			items[a] = carousel.get(a + 1);		}		jQuery.data(carousel, 'items', items);		jQuery(id+' .jcarousel-control a').bind('click', function() {			carousel.scroll(jQuery.jcarousel.intval(jQuery(this).attr('rev') ? jQuery(this).attr('rev') : jQuery(this).text()));			return false;		});		carousel.buttonNext.bind('click', function() {			carousel.startAuto(0);		});		carousel.buttonPrev.bind('click', function() {			carousel.startAuto(0);		});	},	/**	 * Mouseover initCallback	 */	initCallbackMouseover: function(carousel, state) {		carousel.clip.hover(function() {			carousel.stopAuto();		}, function() {			carousel.startAuto();		});	}};
+var imagecarousel = {
+	itemVisibleInCallback: function(carousel, item, i, state, evt) {
+		var items = jQuery.data(carousel, 'items');
+		var idx = carousel.index(i, items.length);
+		carousel.add(i, items[idx - 1].html());
+	},
+	itemVisibleOutCallback: function(carousel, item, i, state, evt) {
+		carousel.remove(i);
+	},
+	randomize: function(carousel, childElem) {
+		var childes = childElem.split(',');
+		var lastChild = childes.pop();
+		var $this = jQuery(carousel + ' ' + childes.join(' '));
+		var elems = $this.children(lastChild);
+		elems.sort(function() { return (Math.round(Math.random())-0.5); });
+		$this.remove(lastChild);
+		for(var i=0; i < elems.length; i++) {
+			$this.append(elems[i]);
+		}
+	},
+	/**
+	 * Normal initCallback
+	 */
+	initCallback: function(id, carousel, state) {
+		var items = [];
+		for (a=0; a<carousel.options.size;a++) {
+			items[a] = carousel.get(a + 1);
+		}
+		jQuery.data(carousel, 'items', items);
+		jQuery(id+' .jcarousel-control a').bind('click', function() {
+			carousel.scroll(jQuery.jcarousel.intval(jQuery(this).attr('rev') ? jQuery(this).attr('rev') : jQuery(this).text()));
+			return false;
+		});
+		carousel.buttonNext.bind('click', function() {
+			carousel.startAuto(0);
+		});
+		carousel.buttonPrev.bind('click', function() {
+			carousel.startAuto(0);
+		});
+	},
+	/**
+	 * Mouseover initCallback
+	 */
+	initCallbackMouseover: function(carousel, state) {
+		carousel.clip.hover(function() {
+			carousel.stopAuto();
+		}, function() {
+			carousel.startAuto();
+		});
+	}
+};
